79178b9c262aacefc6e066def50bef147a01348a
sujal kumar <sujalkumar0444@gmail.com>
2024-11-17 09:12:34 +0530

    deploy url added

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
  export default  {
-    // "SERVER_URL": "https://feel-seven.vercel.app",
-    "SERVER_URL": "http://localhost:8800",
+    "SERVER_URL": "https://assignment-backend-three.vercel.app",
+    // "SERVER_URL": "http://localhost:8800",
 }
